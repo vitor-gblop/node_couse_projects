29ddb8d2074a562bdca32d93e50b3d65bab94e7c
vitor-gblop <vitor.gblop@gmail.com>
2026-05-25 10:27:29 -0500

    controlador de autenticação

---
+++ thougths/controller/authController.js
@@ -0,0 +1,10 @@
+class AuthController {
+  static login(req, res) {
+    res.render("auth/");
+  }
+  static register(req, res) {
+    res.render("auth/");
+  }
+}
+
+module.exports = AuthController;
