8070c8de862b0c6b50258f9fcef08f3b6c533ce0
vitor-gblop <vitor.gblop@gmail.com>
2026-05-26 12:20:05 -0500

    rotas e controlador de registro de usuario

--- thougths/controller/authController.js
+++ thougths/controller/authController.js
@@ -1,3 +1,7 @@
+const { request, response } = require("express");
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
 class AuthController {
   static login(req, res) {
     res.render("auth/login");
@@ -5,6 +9,56 @@ class AuthController {
   static register(req, res) {
     res.render("auth/register");
   }
+  static async _register(req = request, res = response) {
+    const { name, email, password, confirmPassword } = req.body;
+
+    // Verificação da senha
+    if (password != confirmPassword) {
+      req.flash("message", "as senhas não coincidem!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Verificar usuario
+    const userExists = await User.findOne({
+      where: { email },
+    });
+    if (userExists) {
+      req.flash("message", "O email ja esta em uso");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // Criptografar senha
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    try {
+      const user = await User.create({
+        name,
+        email,
+        password: hashPassword,
+      });
+
+      // session
+      // @ts-ignore erro de propriedade
+      req.session.userId = user.id;
+
+      req.flash("message", "Registro realizado");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+
+      res.redirect("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
 }
 
 module.exports = AuthController;
--- thougths/routes/authRoutes.js
+++ thougths/routes/authRoutes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const AuthController = require("../controller/authController");
 const authRouter = express.Router();
 
+// Frontend
 authRouter.get("/login", AuthController.login);
 authRouter.get("/register", AuthController.register);
+// Backend
+authRouter.post("/register", AuthController._register);
 
 module.exports = authRouter;
